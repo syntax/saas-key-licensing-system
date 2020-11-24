0d76b5ddd6ea3e0c5a0102a496b09c752007e1b2
tom <tfeholland@gmail.com>
2020-11-24 13:06:45 +0000

    [testing]  GET requests

--- api.py
+++ api.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
 
+# TESTING PLAYING ABOUT WITH FLASK AND RESTFUL API
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+licenses = [{'id': 1}, {'id': 2}]
+
+
+@app.route('/api/v1/licenses', methods=['GET'])
+def get_licenses():
+    return jsonify({'licenses':licenses})
+
+@app.route('/api/v1/licenses/<int:licenseid>', method=['GET'])
+def get_specific_license(licenseid):
+    license = [license for license in licenses if license['id'] == licenseid]
+    return jsonify({'license':license[0]})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
