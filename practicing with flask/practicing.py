f84e42c8ce3b655d01ec66f5d5fcf4497d28b5e9
tom <tfeholland@gmail.com>
2021-01-02 15:52:24 +0000

    login page looks nice + works well!

--- practicing with flask/practicing.py
+++ practicing with flask/practicing.py
@@ -8,15 +8,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/home')
+def home():
+    return 'success'
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
+        print(request.form)
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
+            error = 'Invalid Credentials. Please try again.\nIf you do not yet have an account, you can sign up with the above link.'
         else:
+            print('redirectign')
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
+
+@app.route('/signup')
+def signup():
+    return 'maintenance'
 
 # @app.route('/singup', methods=['GET', 'POST'])
 # def login():
