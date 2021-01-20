261a4eae3c9c8f0f7ed6e1ebaaf583c6a5fea7e9
tom <tfeholland@gmail.com>
2021-01-20 09:09:38 +0000

    added sripts

--- static/scripts.js
+++ static/scripts.js
@@ -1,10 +1,10 @@
 function relayUserTime(user){
     var time = new Date().getHours();
     if (0<time && time<12) {
-        document.getElementById("time").innerHTML = "Good morning {0}!".format(user);
+        document.getElementById("time").innerHTML = "Good morning" + user + "!" ;
     } else if (12<time && time<17) {
-        document.getElementById("time").innerHTML = "Good farhter {0}!".format(user);
+        document.getElementById("time").innerHTML = "Good afternoon" + user + "!" ;
     } else {
-        document.getElementById("time").innerHTML = "Good cock {0}!".format(user);
+        document.getElementById("time").innerHTML = "Good evening" + user + "!" ;
     }
 }
