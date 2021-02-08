5c7d724bd88c5a5407b8e25b3f7ec1c8f4d47974
tom <tfeholland@gmail.com>
2021-02-08 12:27:56 +0000

    rewrote some of the js functions

--- static/scripts.js
+++ static/scripts.js
@@ -1,10 +1,20 @@
 function relayUserTime(user){
     var time = new Date().getHours();
-    if (0<time && time<12) {
-        document.getElementById("time").innerHTML = "Good morning" + user + "!" ;
-    } else if (12<time && time<17) {
-        document.getElementById("time").innerHTML = "Good afternoon" + user + "!" ;
+    if (0<=time && time<12) {
+        document.getElementById("time").innerHTML = "Good morning " + user + " !" ;
+    } else if (12<= time && time<17) {
+        document.getElementById("time").innerHTML = "Good afternoon " + user + " !" ;
     } else {
-        document.getElementById("time").innerHTML = "Good evening" + user + "!" ;
+        document.getElementById("time").innerHTML = "Good evening " + user + " !" ;
     }
 }
+
+function unbind() {
+    $.get('/unbindaccount');
+    setTimeout(function(){ location.reload(); }, 100);
+}
+
+function redirectFunct() {
+   location.replace("http://127.0.0.1:5000/login")
+   }
+
