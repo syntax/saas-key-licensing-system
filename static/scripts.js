a349ae0e77ab7e166ba9465bc38fc809fed5ad51
tom <tfeholland@gmail.com>
2021-03-25 14:41:47 +0000

    js to indep file

--- static/scripts.js
+++ static/scripts.js
@@ -18,3 +18,17 @@ function redirectFunct() {
    location.replace("http://127.0.0.1:5000/login")
    }
 
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  if (typeof(Storage) !== "undefined") {
+        localStorage.setItem("sidebar", "opened");
+    }
+}
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  if (typeof(Storage) !== "undefined") {
+        localStorage.setItem("sidebar", "closed");
+    }
+}
