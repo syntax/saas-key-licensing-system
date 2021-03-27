b1b7388f2cbe453a4db0ef418970505b55721888
tom <tfeholland@gmail.com>
2021-03-27 14:22:17 +0000

    progressing with merge sort, now works on 2D array.
    now just need to implement:
    
    changing order of rows based on the numeric INDEX value, not teh absolute value of the thing in question

--- mergesort.js
+++ mergesort.js
@@ -83,9 +83,9 @@ function sortTable(n) {
   }
 }
 
-
+//ONE WAY SORT
 function sortTable2(col) {
-    var table, rows, preswitch = [], switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    let table, rows, preswitch = [], switching, i, x, y, shouldSwitch, dir, switchcount = 0;
     table = document.getElementById("dbtable");
     rows = table.rows;
     let tbody = document.querySelector("#dbtable > tbody");
@@ -145,5 +145,80 @@ function sortTable2(col) {
     tbody.parentElement.replaceChild(new_tbody, tbody);
 }
 
-array = [4, 8, 7, 2, 11, 1, 3];
-console.log(mergeSort(array));
+// TWO WAY SORT
+function sortTable2(col) {
+      if (lastCol == undefined) lastCol = col;
+      if (dir == undefined) dir = "asc";
+      if (lastCol != col) {
+        dir = "asc";
+        lastCol = col;
+      }
+      let table, rows, preswitch = [],
+        switching, i, x, y, shouldSwitch, switchcount = 0;
+      table = document.getElementById("dbtable");
+      rows = table.rows;
+      let tbody = document.querySelector("#dbtable > tbody");
+      let new_tbody = tbody.cloneNode();
+      //set direction to ascending initially
+      if (dir == "asc") {
+        dir = "desc"
+      } else {
+        dir = "asc"
+      }
+
+      for (i = 1; i < (rows.length); i++) {
+        preswitch.push([rows[i].getElementsByTagName("TD")[0].innerHTML,rows[i].getElementsByTagName("TD")[col].innerHTML])
+      }
+      //copy of preswitch
+      console.log(preswitch)
+      let preswitch_copy = [...preswitch];
+
+      function merge(left, right) {
+        let array = []
+        // Break out of loop if any one of the array gets empty
+        while (left.length && right.length) {
+          // Pick the smaller among the smallest element of left and right sub arrays
+          if (left[0][1] < right[0][1]) {
+            array.push(left.shift())
+          } else {
+            array.push(right.shift())
+          }
+        }
+
+        // Concatenating the leftover elements
+        // (in case we didn't go through the entire left or right array)
+        // ... acts as a spread operator
+        return [...array, ...left, ...right]
+      }
+
+      function mergeSort(array) {
+        const halflength = array.length / 2
+
+        // Base case
+        if (array.length < 2) {
+          return array
+        }
+
+        const left = array.splice(0, halflength)
+        return merge(mergeSort(left), mergeSort(array))
+      }
+
+      sorted = mergeSort(preswitch);
+      console.log(sorted);
+      preswitch = preswitch_copy;
+      // console.log(preswitch);
+      // console.log(sorted);
+      let index_array = [];
+      // get the order of the sorted array based on the preswitch array
+      sorted.forEach(value => {
+        index_array.push(preswitch.indexOf(value));
+        // console.log(index_array);
+      })
+      console.log(index_array);
+      if(dir=="desc")index_array.reverse();
+      index_array.forEach(index => {
+        // console.log(tbody.children[index].children[1].innerHTML);
+        new_tbody.appendChild(tbody.children[index].cloneNode(true));
+      })
+      tbody.parentElement.replaceChild(new_tbody, tbody);
+    }
